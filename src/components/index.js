2a34951173279270d3b2ee7c49c1df7a6d03f998
MunDo12138 <66548936+MunDo12138@users.noreply.github.com>
2023-05-22 20:15:25 +0800

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import ChannelDetail from './ChannelDetail'
+import Feed from './Feed'
+import Navbar from './Navbar'
+import SearchFeed from './SearchFeed'
+import VideoDetail from './VideoDetail'
+import SearchBar from './SearchBar'
+import Sidebar from './Sidebar'
+import Videos from './Videos'
+import VideoCard from './VideoCard'
+import ChannelCard from './ChannelCard'
+
+export {
+    ChannelDetail,
+    Feed,
+    Navbar,
+    SearchFeed,
+    VideoDetail,
+    SearchBar,
+    Sidebar,
+    Videos,
+    VideoCard,
+    ChannelCard
+}
